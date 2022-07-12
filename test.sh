156d45ecce3b1343df9bc25653afc27d1337550e
Bill.Zhuang <zhuangmin@gmail.com>
2022-07-12 19:51:34 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -5,5 +5,5 @@ echo "::set-output name=$1/$2::$2"
 allThreads=(1 2 4 8 16 32 64 128)
 abc="1, 2,   4, 5"
 
-#echo "::set-output name=arr::${allThreads[*]}"
-echo "::set-output name=arr::${abc}"
+echo "::set-output name=arr::["12","14","16"]}"
+#echo "::set-output name=arr::${abc}"
