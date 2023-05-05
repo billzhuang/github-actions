c8e76813b7b62503b5808a37bae05c3cd2dd6e7f
Bill.Zhuang <zhuangmin@gmail.com>
2023-05-05 16:22:47 +0800

    Update test.sh with new "$GITHUB_OUTPUT"

--- test.sh
+++ test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo "::set-output name=$1/$2::$3"
+echo "name=$1/$2=$3" >> "$GITHUB_OUTPUT"
 
 allThreads=(1 2 4 8 16 32 64 128)
 abc="1, 2,   4, 5"
 
-echo "::set-output name=arr::["12","14","16"]}"
+echo "arr=["12","14","16"]}" >> "$GITHUB_OUTPUT"
 #echo "::set-output name=arr::${abc}"
